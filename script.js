9005ac2407eb96dc91e158bffcb0f14115bd92ee
robert-gherman <62173950+robert-gherman@users.noreply.github.com>
2021-06-30 16:07:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const addForm = document.querySelector(".add");
 const list = document.querySelector(".list-group");
 const search = document.querySelector(".search");
+var storage = [];
 
+window.onload = function () {
+  if (localStorage.getItem("list-group-item") != null)
+    storage = JSON.parse(localStorage.getItem("list-group-item"));
+
+  for (var i = 0; i < storage.length; i++) {
+    if (storage[i] != "") {
+      let htmlTemplate2 = `<li class="list-group-item flex justify-content-between align-items-center">
+      <span class="text text-light">${storage[i]}</span>
+      <input class="form-check-input check my-2 mx-2" type="checkbox" />
+      <i class="fas fa-trash-alt mx-4"></i>
+    </li>`;
+      list.innerHTML += htmlTemplate2;
+    }
+  }
+};
 addForm.addEventListener("submit", (i) => {
   i.preventDefault();
   let addTodo = addForm.add.value;
@@ -15,19 +31,31 @@ addForm.addEventListener("submit", (i) => {
     list.innerHTML += htmlTemplate;
     addForm.reset();
   }
+
+  if (addTodo != "") {
+    storage.push(addTodo);
+    localStorage.setItem("list-group-item", JSON.stringify(storage));
+  }
 });
 
 // delete todo element
 list.addEventListener("click", (i) => {
   if (i.target.tagName == "I") {
+    let v = JSON.parse(localStorage.getItem("list-group-item"));
+
+    for (var k = 0; k < v.length; k++) {
+      if (v[k] == i.target.parentElement.innerText) v.splice(k, 1);
+    }
+
+    localStorage.setItem("list-group-item", JSON.stringify(v));
     i.target.parentElement.remove();
   }
 
-  // pt buton input linie
+  // pt buton linie orizontala
   if (i.target.tagName == "INPUT") {
     const parenta = i.target.parentElement;
     let arr = Array.from(parenta.children);
-
+    console.log(arr[0]);
     if (
       arr[0].style.textDecoration == "" ||
       arr[0].style.textDecoration == "none"
